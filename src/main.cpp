ae0d28952d02ba45b13468af2900dd40d3d85b9c
krmbd <87835614+krmbd@users.noreply.github.com>
2023-04-22 01:46:15 +0200

    Put together an example main program that uses Point and Vector

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
 
+#include "point.hpp"
+#include "vector.hpp"
+
+struct Projectile
+{
+    Point position;
+    Vector velocity;
+};
+
+struct Environment
+{
+    Vector gravity;
+    Vector wind;
+};
+
+Projectile tick(Environment env, Projectile proj)
+{
+    const Point position = proj.position + proj.velocity;
+    const Vector velocity = proj.velocity + env.gravity + env.wind;
+
+    return Projectile{ position, velocity };
+}
+
 int main()
 {
-    std::cout << "Hello, world!\n";
+    Projectile proj{ Point{ 0, 1, 0 }, Normalize(Vector{ 1, 1, 0 }) };
+    const Environment env{ Vector{ 0, -0.1, 0 }, Vector{ -0.01, 0, 0 } };
+
+    unsigned int turn{ 0 };
+    while (proj.position.y > 0)
+    {
+        std::cout << "Turn " << turn << ": The projectile is at position " << proj.position << "\n";
+        proj = tick(env, proj);
+        turn++;
+    }
+
     return 0;
 }

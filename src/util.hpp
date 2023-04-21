df6a4586e19ba71e7c604f40edcd067f203ea592
krmbd <87835614+krmbd@users.noreply.github.com>
2023-04-21 23:22:02 +0200

    Fix typo

--- src/util.hpp
+++ src/util.hpp
@@ -2,9 +2,9 @@
 
 #include <cmath>
 
-const double EPISLON = 0.0001;
+const double EPSILON = 0.0001;
 
 inline bool AreAlmostEqual(double lhs, double rhs)
 {
-    return std::fabs(lhs - rhs) < EPISLON;
+    return std::fabs(lhs - rhs) < EPSILON;
 }
